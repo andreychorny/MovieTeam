7e536a2f74866aa38336ea15bf87d752de09517b
andreychorny <andrey-evil@ukr.net>
2020-01-11 21:33:22 +0200

    CORS support

--- src/main/java/com/movierecommendersystem/movieteam/restcontroller/HomeRestController.java
+++ src/main/java/com/movierecommendersystem/movieteam/restcontroller/HomeRestController.java
@@ -5,6 +5,7 @@ import com.movierecommendersystem.movieteam.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeRestController {
 
     @Autowired
